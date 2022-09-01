963519d2e36b295cddc790c4d783a117f8dd1d66
Dhilip KARTHIKEYAN <74380649+K-Dhilip@users.noreply.github.com>
2022-09-01 15:20:15 +0200

    update

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\component\Form\Extension\Core\Type\TextType;
 
 class BlogController extends AbstractController
 {
@@ -18,19 +23,51 @@ class BlogController extends AbstractController
             'age' => 25
         ]);
     }
-        /**
+    /**
      * @Route("/blog/index", name="blog_index")
      */
-    public function index(): Response
+    public function index(ArticleRepository $repo): Response
     {
-        return $this->render('blog/index.html.twig');
+
+        $article = $repo->findBy(array(), array('id' => 'ASC'));
+
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $articles = $repo->findAll();
+
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+            'articles' => $articles
+        ]);
+    }
+    /**
+     * @Route("/blog/new", name="blog_create")
+     */
+    public function create(Request $request, EntityManagerInterface $manager)
+    {
+        $article = new Article(); // nous déclarons un article qui est vide mais pret à être rempli
+        // $form est un objet complexe, nous allons demander à symfony de nous stocker le formulaire dans une variable simple à utilier.
+        $form = $this->createFormBuilder($article) // cela va créer un objet qui est lié à notre article
+            ->add('title') // add() fonction permettant de créer des champs dans un formulaire
+            ->add('content', textType::class)
+            ->add('image')
+            ->getForm(); // permet d'afficher le rendu final
+        return $this->render('blog/create.html.twig', [
+            // createView() va retourner un petit objet qui représente l'affichage du formulaire, on le récupère sur la page create.html.twig
+            'formArticle' => $form->createView()
+        ]);
     }
 
+
     /**
-     * @Route("/blog/12", name="blog_show")
+     * @Route("/blog/{id}", name="blog_show")
      */
-    public function show(): Response
+    public function show($id)
     {
-        return $this->render('blog/show.html.twig');
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $article = $repo->find($id);
+        return $this->render('blog/show.html.twig', [
+            'article' => $article
+
+        ]);
     }
 }
